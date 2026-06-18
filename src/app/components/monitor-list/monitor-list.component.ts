4366d9d76313856c9ae6cfaf6faf1b1f4918fac7
David Santana <davidsantana06@hotmail.com>
2026-06-18 13:51:24 -0300

    feat: add monitor list (placeholder) component

---
+++ src/app/components/monitor-list/monitor-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-monitor-list',
+  templateUrl: './monitor-list.component.html',
+  styleUrl: './monitor-list.component.css'
+})
+export class MonitorListComponent {
+
+}
