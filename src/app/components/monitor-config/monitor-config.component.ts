857c0a754f0bbcb16e613115e8037e1c44ed381b
David Santana <davidsantana06@hotmail.com>
2026-06-18 13:51:17 -0300

    feat: add monitor config (placeholder) component

---
+++ src/app/components/monitor-config/monitor-config.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-monitor-config',
+  templateUrl: './monitor-config.component.html',
+  styleUrl: './monitor-config.component.css'
+})
+export class MonitorConfigComponent {
+
+}
