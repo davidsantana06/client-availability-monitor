3e48113d0a390635a5e0d47aa36a1300fc547efb
David Santana <davidsantana06@hotmail.com>
2026-06-18 14:32:05 -0300

    feat: add users info component

--- src/app/components/users-info/users-info.component.ts
+++ src/app/components/users-info/users-info.component.ts
@@ -1,8 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+
+import { StorageService } from '../../services/storage.service';
+import { User, UsersInfo } from '../../models/users-info.model';
+import { email } from '../../validators/app-validators';
 
 @Component({
   selector: 'app-users-info',
   templateUrl: './users-info.component.html',
   styleUrl: './users-info.component.css',
 })
-export class UsersInfoComponent {}
+export class UsersInfoComponent implements OnInit {
+  private readonly fb = inject(FormBuilder);
+  private readonly storage = inject(StorageService);
+
+  readonly users$: Observable<UsersInfo> = this.storage.usersInfo$;
+  form!: FormGroup;
+  editingIndex: number | null = null;
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, email]],
+    });
+  }
+
+  invalid(control: string): boolean {
+    const target = this.form.get(control);
+    return !!target && target.invalid && target.touched;
+  }
+
+  edit(index: number): void {
+    const user = this.storage.usersInfo[index];
+    this.form.setValue({ username: user.username, email: user.email });
+    this.editingIndex = index;
+  }
+
+  remove(index: number): void {
+    this.storage.setUsersInfo(this.storage.usersInfo.filter((_, i) => i !== index));
+    if (this.editingIndex !== null) {
+      this.cancel();
+    }
+  }
+
+  submit(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    const user: User = this.form.getRawValue();
+    const users = [...this.storage.usersInfo];
+    if (this.editingIndex === null) {
+      users.push(user);
+    } else {
+      users[this.editingIndex] = user;
+    }
+    this.storage.setUsersInfo(users);
+    this.cancel();
+  }
+
+  cancel(): void {
+    this.form.reset({ username: '', email: '' });
+    this.editingIndex = null;
+  }
+}
