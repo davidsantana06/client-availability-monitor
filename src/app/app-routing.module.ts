e68778a12ad85c1711655175166c7495e60937ca
David Santana <davidsantana06@hotmail.com>
2026-06-18 13:51:58 -0300

    feat: configure routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { MonitorConfigComponent } from './components/monitor-config/monitor-config.component';
+import { UsersInfoComponent } from './components/users-info/users-info.component';
+import { ServersPoolComponent } from './components/servers-pool/servers-pool.component';
+import { MonitorListComponent } from './components/monitor-list/monitor-list.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'monitor-config', pathMatch: 'full' },
+  { path: 'monitor-config', component: MonitorConfigComponent },
+  { path: 'users-info', component: UsersInfoComponent },
+  { path: 'servers-pool', component: ServersPoolComponent },
+  { path: 'monitor-list', component: MonitorListComponent },
+  { path: '**', redirectTo: 'monitor-config' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MonitorConfigComponent } from './components/monitor-config/monitor-config.component';
+import { UsersInfoComponent } from './components/users-info/users-info.component';
+import { ServersPoolComponent } from './components/servers-pool/servers-pool.component';
+import { MonitorListComponent } from './components/monitor-list/monitor-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MonitorConfigComponent,
+    UsersInfoComponent,
+    ServersPoolComponent,
+    MonitorListComponent
   ],
   imports: [
     BrowserModule,
