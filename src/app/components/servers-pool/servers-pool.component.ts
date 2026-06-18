7fe6cdcd8a0a1db2551894d8f3c35adbc72f6484
David Santana <davidsantana06@hotmail.com>
2026-06-18 13:51:34 -0300

    feat: add servers pool (placeholder) component

---
+++ src/app/components/servers-pool/servers-pool.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers-pool',
+  templateUrl: './servers-pool.component.html',
+  styleUrl: './servers-pool.component.css'
+})
+export class ServersPoolComponent {
+
+}
