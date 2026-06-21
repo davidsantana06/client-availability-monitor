52215b789e5ce152f9443980a2a6aac1488d3589
David Santana <davidsantana06@hotmail.com>
2026-06-21 13:22:51 -0300

    feat: reject duplicate emails on import

--- src/app/validators/users-info.validators.ts
+++ src/app/validators/users-info.validators.ts
@@ -1,8 +1,12 @@
-import { isNonEmptyString, isRecord, isValidEmail } from './app-validators';
+import { hasUniqueValues, isNonEmptyString, isRecord, isValidEmail } from './app-validators';
 import { User, UsersInfo } from '../models/users-info.model';
 
 export function isUsersInfo(value: unknown): value is UsersInfo {
-  return Array.isArray(value) && value.every(isUser);
+  return (
+    Array.isArray(value) &&
+    value.every(isUser) &&
+    hasUniqueValues(value.map((user) => user.email))
+  );
 }
 
 function isUser(value: unknown): value is User {
