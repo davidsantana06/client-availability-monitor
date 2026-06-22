c8ba7fd4aeeafe6799e60cf78ebd8ec136494515
David Santana <davidsantana06@hotmail.com>
2026-06-22 20:23:36 -0300

    fix: exclude the edited server from hostname uniqueness

--- src/app/components/servers-pool/servers-pool.component.ts
+++ src/app/components/servers-pool/servers-pool.component.ts
@@ -108,8 +108,8 @@ export class ServersPoolComponent implements OnInit {
   }
 
   edit(index: number): void {
-    this.fillForm(this.service.value[index]);
     this.editingIndex = index;
+    this.fillForm(this.service.value[index]);
   }
 
   cancel(): void {
